a30d7306589bfa85355ac759b5d1f34aff65440e
shuminwang <wsmin123456@hotmail.com>
2022-07-31 16:49:54 -0700

    documents

--- src/main/persistence/JsonWriter.java
+++ src/main/persistence/JsonWriter.java
@@ -27,7 +27,7 @@ public class JsonWriter {
     }
 
     // MODIFIES: this
-    // EFFECTS: writes JSON representation of workroom to file
+    // EFFECTS: writes JSON representation of typeList to file
     public void write(TypeList typeList) {
         JSONArray jsonArray = new JSONArray();
         for (Type t : typeList.getTypes()) {
